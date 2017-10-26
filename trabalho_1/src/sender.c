fa04e6c21e7dea79e316a6845872fd1119dc38f2
NadiaCarvalho <nadiacarvalho118@gmail.com>
2017-10-26 17:45:03 +0100

    Changed things in lines 49,52&53 in dataLink.c so i can work from my linux on vbox. It needs to be changed for “/dev/ttySx” to work on school computer

--- trabalho_1/src/sender.c
+++ trabalho_1/src/sender.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   printf("llopen complete\n");
 
   char buffer[255];
-  llwrite(fd, buffer, 255);
+  llwrite(fd, buffer, 25);
   printf("llwrite complete\n");
 
   // llclose();
